c93ef7bcdee866e267dd5d7f9be9a2ef9c49a651
teppy <tepong32@gmail.com>
2024-08-20 15:29:09 +0800

    added django-allauth and related-settings

--- djangoCustomUserTemplate/src/settings.py
+++ djangoCustomUserTemplate/src/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +32,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    ### third-party
+    'crispy_forms',
 
+    # django-allauth needs this
+    'django.contrib.sites', 
+    # below are everything all-auth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # ... include the providers you want to allauth to enable:
+    # 'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.twitter',
+    # add more if you wish to
 
     ### defaults
     'django.contrib.admin',
@@ -46,6 +60,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'allauth.account.middleware.AccountMiddleware', # for django-allauth
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,10 +136,66 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")     # where we want django to save uploaded files
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.User'
+
+
+
+################################################### ALL OTHER ADDITIONAL CUSTOM SETTINGS
+AUTH_USER_MODEL = 'users.User' # for the project to use the custom User we created instead of the default
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # for the css of crispy forms
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
+# TO USE THESE VARIABLES BELOW, USE ENVIRONMENT VARIABLES TO HIDE SENSITIVE INFO
+# CHECK CoreyMs' Django TUTORIAL # 12 -- 14:20
+EMAIL_HOST_USER = os.environ.get('ADMIN_EMAIL_UN') # var for email username
+EMAIL_HOST_PASSWORD = os.environ.get('ADMIN_EMAIL_PW') # var for email pw
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER # for email-sending pw-reset requests
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' # or only your domain name if you have your own mail server
+EMAIL_PORT = 587 #587
+EMAIL_USE_TLS = True
+
+
+
+
+
+# django-allauth needs this
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+# Provider specific settings for django-allauth
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id': os.environ.get('GAUTH_CLIENTID'), # '960711795493-9vsgskaeg1qk3nc74qp27s9e7uoejitq.apps.googleusercontent.com'
+            'secret': os.environ.get('GAUTH_SECRET'),      # '0-tHwlg4jvax1jt7p-JnvBmj'
+            'key': ''
+        }
+    }
+}
+
+
+# edit this for customized ckeditor toolbar options
+# CKEDITOR_UPLOAD_PATH = "ckeditor_uploads/"
+# CKEDITOR_CONFIGS = {
+#     'awesome_ckeditor': {
+#         'toolbar': 'Basic',
+#     },
+# }
+
--- djangoCustomUserTemplate/src/urls.py
+++ djangoCustomUserTemplate/src/urls.py
@@ -21,3 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 ]
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
